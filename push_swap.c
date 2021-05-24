43cdf434efd7592b59c8dd7f22674650c74ed626
TrincaMelros <malmeida@student.42lisboa.com>
2021-05-24 13:17:11 +0100

    Started work on swap and reverse instructions

--- push_swap.c
+++ push_swap.c
@@ -6,61 +6,89 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:02:32 by malmeida          #+#    #+#             */
-/*   Updated: 2021/05/19 15:40:30 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/05/24 12:34:43 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-/*
-void	rotate(int *a, int i)
-{
-	int j;
-	int	temp;
 
-	j = 0;
-	temp = a[0];
-	while (j < i)
+void	push(int **a, int **b, int i)
+{
+	int f;
+
+	f = 0;
+	while (f < i)
 	{
-		
+		a[f] = a[f + 1];
+		f++;
 	}
 }
-*/
-void	swap(int *a)
+
+void	swap(int **a)
 {
-	int temp;
+	int *temp;
 
 	temp = a[0];
 	a[0] = a[1];
 	a[1] = temp;
 }
 
-void	printer(int *a, int i)
+void	start_stack(int **a, int i)
+{
+
+	while (i)
+	{
+		a[i - 1] = NULL;
+		i--;
+	}
+}
+
+void	printer(int **a, int **b, int i)
 {
 	int j;
 
 	j = 0;
 	while (j < i)
 	{
-		printf("%d\n", a[j]);
+		if (b[j] == NULL && a[j] != NULL)
+			printf("%d __ N\n", *a[j]);
+		else if (a[j] == NULL && b[j] != NULL)
+			printf("N __ %d\n", *b[j]);
+		else if (a[j] == NULL && b[j] == NULL)
+			printf("N __ N\n");
+		else
+			printf("%d __ %d\n", *a[j], *b[j]);
 		j++;
 	}
+	printf("\nA __ B\n");
+}
+
+int 	*add_stack(int i)
+{
+	int *x;
+
+	x = malloc(sizeof(int *));
+	*x = i;
+	return (x);
 }
 
 int	main(int argc, char **argv)
 {
 
-	int	stackA[argc - 1];
+	int	*stackA[argc - 1];
+	int *stackB[argc - 1];
 	int	i;
 
 	i = 0;
 	while (argc - 1 - i)
 	{
-		stackA[i] = ft_atoi(argv[1 + i]);
+		stackA[i] = add_stack(ft_atoi(argv[1 + i]));
 		i++;
 	}
-	printer(stackA, i);
+	start_stack(stackB, i);
+	printer(stackA, stackB, i);
 	printf("________________\n");
 	swap(stackA);
-	printer(stackA, i);
+	printer(stackA, stackB, i);
 	return (0);
 }
