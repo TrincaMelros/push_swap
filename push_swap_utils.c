ffcc282a426d26ff63851b8ce5711fd570f0171a
TrincaMelros <malmeida@student.42lisboa.com>
2021-05-25 16:44:15 +0100

    Utility functions moved to new file

---
+++ push_swap_utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 16:26:24 by malmeida          #+#    #+#             */
+/*   Updated: 2021/05/25 16:42:04 by malmeida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	start_stack(int **a, int i)
+{
+	while (i)
+	{
+		a[i - 1] = NULL;
+		i--;
+	}
+}
+
+void	*add_stack(int i)
+{
+	int	*x;
+
+	x = malloc(sizeof(int *));
+	*x = i;
+	return (x);
+}
+
+void	stack_printer(int **a, int **b, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		if (a[j] != NULL && b[j] == NULL)
+			printf("%d __ N\n", *a[j]);
+		else if (a[j] == NULL && b[j] != NULL)
+			printf("N __ %d\n", *b[j]);
+		else if (a[j] == NULL && b[j] == NULL)
+			printf("N __ N\n");
+		else
+			printf("%d __ %d\n", *a[j], *b[j]);
+		j++;
+	}
+	printf("\nA __ B\n");
+}
+
+int		ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	number;
+
+	i = 0;
+	while (nptr[i] && ((nptr[i] >= 9 && nptr[i] <= 13) || (nptr[i] == 32)))
+		i++;
+	sign = 1;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	number = 0;
+	while (nptr[i] && (nptr[i] >= '0' && nptr[i] <= '9'))
+	{
+		number *= 10;
+		number =+ nptr[i] - '0';
+		i++;
+	}
+	return (number * sign);
+}
