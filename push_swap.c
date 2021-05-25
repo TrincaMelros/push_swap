d869181e083823ad6256560fb7db6b4912656241
TrincaMelros <malmeida@student.42lisboa.com>
2021-05-25 16:43:32 +0100

    Re-organized functions into different files

--- push_swap.c
+++ push_swap.c
@@ -6,50 +6,11 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:02:32 by malmeida          #+#    #+#             */
-/*   Updated: 2021/05/25 14:59:54 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/05/25 16:34:28 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	start_stack(int **a, int i)
-{
-
-	while (i)
-	{
-		a[i - 1] = NULL;
-		i--;
-	}
-}
-
-void	printer(int **a, int **b, int i)
-{
-	int j;
-
-	j = 0;
-	while (j < i)
-	{
-		if (b[j] == NULL && a[j] != NULL)
-			printf("%d __ N\n", *a[j]);
-		else if (a[j] == NULL && b[j] != NULL)
-			printf("N __ %d\n", *b[j]);
-		else if (a[j] == NULL && b[j] == NULL)
-			printf("N __ N\n");
-		else
-			printf("%d __ %d\n", *a[j], *b[j]);
-		j++;
-	}
-	printf("\nA __ B\n");
-}
-
-int 	*add_stack(int i)
-{
-	int *x;
-
-	x = malloc(sizeof(int *));
-	*x = i;
-	return (x);
-}
 
 int	main(int argc, char **argv)
 {
@@ -65,12 +26,12 @@ int	main(int argc, char **argv)
 		i++;
 	}
 	start_stack(stackB, i);
-	printer(stackA, stackB, i);
+	stack_printer(stackA, stackB, i);
 	printf("________________\n");
 	reverse_rotate(stackA, i);
-	printer(stackA, stackB, i);
+	stack_printer(stackA, stackB, i);
 	rotate(stackA, i);
-	printer(stackA, stackB, i);
+	stack_printer(stackA, stackB, i);
 	printf("\n%d\n", i);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 12:53:55 by malmeida          #+#    #+#             */
-/*   Updated: 2021/05/25 15:01:30 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/05/25 16:35:37 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ void	push(int **a, int **b, int i);
 void	rotate(int **a, int i);
 void	reverse_rotate(int **a, int i);
 
+void	start_stack(int **a, int i);
+void	*add_stack(int i);
+void	stack_printer(int **a, int **b, int i);
 int		ft_atoi(const char *nptr);
 
 #endif
